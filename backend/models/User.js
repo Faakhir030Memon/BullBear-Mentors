17157d50ef0bff60317aeb940850bbcaebf6d754
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-04-29 08:34:38 +0500

    feat: create User mongoose model with password hashing and authentication methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,8 +13,11 @@ const userSchema = mongoose.Schema({
     loginHistory: [{
         loginTime: { type: Date, default: Date.now },
         ip: String
-    }]
+    }],
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
 }, { timestamps: true });
+
 
 // Hash password before saving
 userSchema.pre('save', async function () {
